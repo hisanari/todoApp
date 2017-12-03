5119a4d83d4f361513536302ccfa806321a2926d
hisanari <hisanari@foobar.com>
2017-12-03 17:55:40 +0900

    テストの追加

---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  include Warden::Test::Helpers
+
+  test 'ユーザー登録失敗' do
+    get new_user_registration_path
+    assert_no_difference 'User.count' do
+      post user_registration_path, params: {
+        user: {
+          email: '',
+          password: 'foo',
+          password_confirmation: 'bar'
+        }
+      }
+    end
+    assert_template 'devise/registrations/new'
+  end
+
+  test 'ユーザー登録成功' do
+    get new_user_registration_path
+    assert_difference 'User.count', 1 do
+      post user_registration_path, params: {
+        user: {
+          email: 'foobar@sample.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'static_pages/home'
+  end
+end
