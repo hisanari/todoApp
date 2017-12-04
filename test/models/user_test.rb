10241201c5429bec53e4f66b1d8f8a7a652bddf7
hisanari <hisanari@foobar.com>
2017-12-04 23:19:12 +0900

    モデルテスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'user@exsample.com', password: 'foobar')
+  end
+
+  test '登録成功' do
+    assert @user.valid?
+  end
+
+  test '名前が空で失敗' do
+    @user.email = '    '
+    assert_not @user.valid?
+  end
+
+  test 'パスワードが空で失敗' do
+    @user.password = '     '
+    assert_not @user.valid?
+  end
+
+  test 'メールアドレスが被ってない' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
