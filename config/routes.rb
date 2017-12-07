f3348f7ce3a863cf7fc869e10c3b01055ce81c0f
hisanari <hisanari@foobar.com>
2017-12-07 22:59:19 +0900

    usersコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resource :users, only: [:show]
   root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get '/help',    to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
-
 end
