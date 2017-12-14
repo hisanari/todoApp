e3ca08eb7955052c6b0baf3f49b20c27a206a16d
hisanari <hisanari@foobar.com>
2017-12-14 22:19:11 +0900

    userコントローラーテスト追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:john)
+  end
+  test 'should get user path' do
+    sign_in @user
+    get users_path
+    assert_response :success
+  end
 end
