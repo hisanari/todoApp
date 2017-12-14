416705a66d0bb2d051523e9463a5956f75e3c182
hisanari <hisanari@foobar.com>
2017-12-14 22:01:10 +0900

    ユーザー編集テスト修正

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -12,15 +12,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_registration_path
     assert_response :success
     assert_template 'devise/registrations/edit'
+    email = 'foo@example.com'
     patch user_registration_path, params: {
       user: {
-        email: @user.email,
-        password: 'foo',
-        password_confirmation: 'bar',
-        current_password: 'foobar'
+        email: email,
+        password: '',
+        password_confirmation: '',
+        current_password: 'miss_password'
       }
     }
     assert_template 'devise/registrations/edit'
+    @user.reload
+    assert_not_equal email, @user.email
   end
 
   test 'ユーザー情報編集成功' do
