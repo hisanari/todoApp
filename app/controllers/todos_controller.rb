41d25e800721c2b7456dd3c68410fae7fb1aa82d
hisanari <hisanari@foobar.com>
2018-01-27 11:02:08 +0900

    Todoコントローラーを修正

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,21 +3,19 @@ class TodosController < ApplicationController
   before_action :todo_limit_update
 
   def index
-    @todos = Todo.where(task_list_id: params[:task_list_id]).order(:todo_limit)
-    @tasklist_title = current_user.task_lists.find_by(
-      id: params[:task_list_id]
-    ).title
+    set_todos
+    set_current_tasklist
     @new_todo = TaskList.find_by(id: params[:task_list_id]).todos.build
   end
 
   def create
-    @new_todo = TaskList.find_by(
-      id: params[:task_list_id]
-    ).todos.build(todo_params)
+    @new_todo = TaskList.find_by(id: params[:task_list_id]).todos.build(todo_params)
+
     if @new_todo.save
       redirect_to task_list_todos_path, notice: '新しいTodoが作成されました。'
     else
-      @todos = Todo.where(task_list_id: params[:task_list_id])
+      set_todos
+      set_current_tasklist
       render 'todos/index'
     end
   end
@@ -31,5 +29,13 @@ class TodosController < ApplicationController
 
   def todo_params
     params.require(:todo).permit(:item, :todo_limit)
+  end
+
+  def set_todos
+    @todos = Todo.where(task_list_id: params[:task_list_id]).order(:todo_limit)
+  end
+
+  def set_current_tasklist
+    @current_tasklist = current_user.task_lists.find_by(id: params[:task_list_id])
   end
 end
