368b7f634f2362177f579d3a5fc3e1eb76d1c5cd
hisanari <hisanari.1987@gmail.com>
2018-04-30 16:06:26 +0900

    検索をヘッダーへ変更、検索機能を自前に変更

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -3,7 +3,11 @@ class SearchPagesController < ApplicationController
 
   # /user/search_pages
   def index
-    @q = TaskList.where(user_id: current_user.id).ransack(params[:q])
-    @result = @q.result(distinct: true).includes(:todos)
+    @task_result = current_user.task_lists.where(
+      'title LIKE ?', "%#{params[:q]}%"
+    )
+    @todo_result = Todo.user_todos(
+      current_user.task_lists.ids
+    ).where('item LIKE ?', "%#{params[:q]}%").includes(:task_list)
   end
 end
