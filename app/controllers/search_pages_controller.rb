8d58dd4179fa07df2a8b3837ef121c486fba1a9a
hisanari <hisanari.1987@gmail.com>
2018-05-01 22:12:00 +0900

    search_pagesのビューのrenderを修正

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -3,10 +3,10 @@ class SearchPagesController < ApplicationController
 
   # /user/search_pages
   def index
-    @task_result = current_user.task_lists.where(
+    @task_results = current_user.task_lists.where(
       'title LIKE ?', "%#{params[:q]}%"
     )
-    @todo_result = Todo.user_todos(
+    @todo_results = Todo.user_todos(
       current_user.task_lists.ids
     ).where('item LIKE ?', "%#{params[:q]}%").includes(:task_list)
   end
