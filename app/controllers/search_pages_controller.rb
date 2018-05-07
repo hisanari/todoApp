bbe82aeccb305f449b9d4823a6ec9a9ecc99d350
hisanari <hisanari.1987@gmail.com>
2018-05-07 21:24:30 +0900

    serch_conttollerにparams_existを追加

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -1,5 +1,6 @@
 class SearchPagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :params_exist
 
   # /user/search_pages
   def index
@@ -9,5 +10,11 @@ class SearchPagesController < ApplicationController
     @todo_results = Todo.user_todos(
       current_user.task_lists.ids
     ).where('item LIKE ?', "%#{params[:q]}%").includes(:task_list)
+  end
+
+  private
+
+  def params_exist
+    params[:q] ||= ''
   end
 end
