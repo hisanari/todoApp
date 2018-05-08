e71ad82a7d84da0063e5bb03da9877f42dbdf44a
hisanari <hisanari.1987@gmail.com>
2018-05-08 22:30:59 +0900

    search controllerのテストを修正

--- test/controllers/search_pages_controller_test.rb
+++ test/controllers/search_pages_controller_test.rb
@@ -14,6 +14,25 @@ class SearchPagesControllerTest < ActionDispatch::IntegrationTest
     assert_template :index
   end
 
+  test '検索できる' do
+    sign_in @user
+    get search_pages_path, params: { q: '家事' }
+    assert_response :success
+    # タスクリストの検索結果があり
+    task_results = assigns[:task_results]
+    assert_equal task_lists(:kaji), task_results.first
+    # TODOの検索結果が無い
+    todo_results = assigns[:todo_results]
+    assert todo_results.empty?
+    get search_pages_path, params: { q: '英語' }
+    # タスクリストの検索結果がない
+    task_results = assigns[:task_results]
+    assert task_results.empty?
+    # TODOの検索結果がある
+    todo_results = assigns[:todo_results]
+    assert_equal todos(:english), todo_results.first
+  end
+
   test 'ログインしていないと見れない' do
     get search_pages_path
     assert_redirected_to new_user_session_path
