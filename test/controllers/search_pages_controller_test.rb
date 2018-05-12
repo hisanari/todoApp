28ca5a2926aba746078eb1f12f34e5ab18234f23
hisanari <hisanari.1987@gmail.com>
2018-05-12 09:31:08 +0900

    検索ページのテスト修正

--- test/controllers/search_pages_controller_test.rb
+++ test/controllers/search_pages_controller_test.rb
@@ -14,23 +14,25 @@ class SearchPagesControllerTest < ActionDispatch::IntegrationTest
     assert_template :index
   end
 
-  test '検索できる' do
+  test 'タスクリストの検索結果がなくて、TODOの検索結果がある' do
+    sign_in @user
+    get search_pages_path, params: { q: '英語' }
+    task_results = assigns[:task_results]
+    assert task_results.empty?
+    todo_results = assigns[:todo_results]
+    assert_equal todos(:english), todo_results.first
+    assert_select "div#todo-id-#{todos(:english).id}"
+  end
+
+  test 'タスクリストの検索結果があり、TODOの検索結果が無い' do
     sign_in @user
     get search_pages_path, params: { q: '家事' }
     assert_response :success
-    # タスクリストの検索結果があり
     task_results = assigns[:task_results]
     assert_equal task_lists(:kaji), task_results.first
-    # TODOの検索結果が無い
+    assert_select "div#result-id-#{task_lists(:kaji).id}"
     todo_results = assigns[:todo_results]
     assert todo_results.empty?
-    get search_pages_path, params: { q: '英語' }
-    # タスクリストの検索結果がない
-    task_results = assigns[:task_results]
-    assert task_results.empty?
-    # TODOの検索結果がある
-    todo_results = assigns[:todo_results]
-    assert_equal todos(:english), todo_results.first
   end
 
   test 'ログインしていないと見れない' do
